a0319bc009adbd3d3d338590784a480ed6a0b115
Scot Mortimer <scot.mortimer@gmail.com>
2017-07-13 15:38:24 -0700

    picture update

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -744,7 +744,7 @@ var _App2 = _interopRequireDefault(_App);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 window.app = new _App2.default();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImZha2VfZWYyNzg1ZWQuanMiXSwibmFtZXMiOlsid2luZG93IiwiYXBwIl0sIm1hcHBpbmdzIjoiOztBQUFBOzs7Ozs7QUFFQUEsT0FBT0MsR0FBUCxHQUFhLG1CQUFiIiwiZmlsZSI6ImZha2VfZWYyNzg1ZWQuanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgQXBwIGZyb20gJy4vYXBwL0FwcCc7XG5cbndpbmRvdy5hcHAgPSBuZXcgQXBwKCk7XG4iXX0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImZha2VfN2Y2Y2UzMTcuanMiXSwibmFtZXMiOlsid2luZG93IiwiYXBwIl0sIm1hcHBpbmdzIjoiOztBQUFBOzs7Ozs7QUFFQUEsT0FBT0MsR0FBUCxHQUFhLG1CQUFiIiwiZmlsZSI6ImZha2VfN2Y2Y2UzMTcuanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgQXBwIGZyb20gJy4vYXBwL0FwcCc7XG5cbndpbmRvdy5hcHAgPSBuZXcgQXBwKCk7XG4iXX0=
 },{"./app/App":1}],13:[function(require,module,exports){
 (function (global){
 /*!
